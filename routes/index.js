f99d33e5f03025d513d50dcfcab0a97b029cedc3
mintmarshmallow <nopp054@naver.com>
2019-09-04 15:44:27 +0900

    tutorial was done!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => res.render("Welcome"));
+const { ensureAuthenticated } = require('../config/auth')
+// Wlecom page
+router.get("/", (req, res) => res.render("welcome"));
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+    res.render("dashboard", {
+        name: req.user.name,
+    })
+});
 
 module.exports = router;
